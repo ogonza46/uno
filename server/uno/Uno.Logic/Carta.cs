a53aa06d6f7421572e90cc9caca1f50eb923b8df
ogonza46 <32881332+ogonza46@users.noreply.github.com>
2021-06-04 11:57:10 +0200

    Tirada test - Proxima tirada logic (#3)
    Tests creados.
    Lógica de próxima tirada en relación a los tests.
    Clase Carta

---
+++ server/uno/Uno.Logic/Carta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uno.Logic
+{
+    public class Carta
+    {
+        private string simbolo;
+        private string color;
+
+        public Carta(string simbolo, string color)
+        {
+            this.simbolo = simbolo ?? throw new ArgumentNullException(nameof(simbolo));
+            this.color = color ?? throw new ArgumentNullException(nameof(color));
+        }
+
+        public string Simbolo
+        {
+            get { return simbolo;  }
+            set { simbolo = value; }
+        }
+
+        public string Color
+        {
+            get { return color;  }
+            set { color = value; }
+        }
+    }
+}
---
+++ server/uno/Uno.Logic/ProximaTirada.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Uno.Logic
+{
+	public class ProximaTirada
+	{
+		public bool PuedoTirar(Carta cartaAnterior, Carta cartaActual)
+		{
+			bool rt = false;
+			//Mismo color
+			if (cartaAnterior.Color.Equals(cartaActual.Color))
+            {
+				rt = true;
+            }
+            //Mismo número
+            if (cartaAnterior.Simbolo.Equals(cartaActual.Simbolo))
+            {
+				rt =  true;
+            }
+			//+4
+			if (cartaActual.Simbolo.Equals("+") && cartaActual.Color.Equals("4"))
+            {
+				rt =  true;
+            }
+			//Cambio color
+			if (cartaActual.Simbolo.Equals("C") && cartaActual.Color.Equals("C"))
+            {
+				rt = true;
+            }
+
+			return rt;
+		}
+	}
+}
